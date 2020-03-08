2ba56a306479307feddaa298386e17b2f2d92419
Aamol Gote <aamolgote@hotmail.com>
2020-03-07 23:47:06 -0500

    Project Ui Adjustments

--- DataVisualization/Week8/ShinyWebApp/FinalProject/ui.R
+++ DataVisualization/Week8/ShinyWebApp/FinalProject/ui.R
@@ -8,27 +8,86 @@
 #
 
 library(shiny)
-
+library(shinythemes)
+library(shinycssloaders)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-    
+    theme = shinytheme("slate"),
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel(""),
+    titlePanel(title=div(style="display:inline-block;width:100%;",
+                         img(src="homelogo.png", style="height:100px;"), "Data analysis (2007-2018)"), 
+               windowTitle = "Lending club data analysis"),
     
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
+            # Price Range filter
+            sliderInput("loanAmount",
+                        "Loan Amount:",
+                        pre = "$",
+                        min = 500,
+                        max = 40000,
+                        value = c(500, 40000)),
+            # Horizontal line ----
+            tags$hr(),
             
+            # House condition filter
+            checkboxGroupInput(inputId = "grade",
+                               label ="Grade:",
+                               choiceNames = c("A", "B", "C", "D", "E", "F", "G"),  
+                               choiceValues = c("A", "B", "C", "D", "E", "F", "G"),
+                               selected = c("A", "B", "C", "D", "E", "F", "G"),
+                               inline = TRUE),
+            # Horizontal line ----
+            tags$hr(),
+            
+            # House condition filter
+            checkboxGroupInput(inputId = "homeOwnership",
+                               label ="Home Ownership:",
+                               choiceNames = c("ANY", "RENT", "MORTGAGE", "OWN"),  
+                               choiceValues = c("ANY", "RENT", "MORTGAGE", "OWN"),
+                               selected = c("ANY", "RENT", "MORTGAGE", "OWN"),
+                               inline = TRUE),
+            # Horizontal line ----
+            tags$hr(),
+            
+
+            # Number of bedrooms filter
+            selectInput("loanStatus", "Loan Status:",
+                        c(
+                            "Any" = "Any",
+                            "Current" = "Current",
+                            "Fully Paid" = "Fully Paid",
+                            "Late (31-120 days)" = "Late (31-120 days)",
+                            "In Grace Period" = "In Grace Period",
+                            "Charged Off" = "Charged Off",
+                            "Late (16-30 days)" = "Late (16-30 days)",
+                            "Default" = "Default",
+                            "Does not meet the credit policy. Status:Fully Paid" = "Does not meet the credit policy. Status:Fully Paid",
+                            "Does not meet the credit policy. Status:Charged Off" = "Does not meet the credit policy. Status:Charged Off"
+                        )),
+            # Horizontal line ----
+            tags$hr(),
+            
+            # Price Range filter
+            sliderInput("dti",
+                        "Debt to Income Ratio:",
+                        pre = "$",
+                        min = -1,
+                        max = 999,
+                        value = c(0, 100)),
+            # Horizontal line ----
+            tags$hr()
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(
-                tabPanel("Loans processed in each Year", plotOutput("loanProcessesedEachYear", height="700px")),
-                tabPanel("Price by Geography", plotOutput("totalFundedLoanAmountEachYear", height="700px")),
-                tabPanel("Loan Amount Term Relation", plotOutput("loanAmtTermRelation", height="700px")),
-                tabPanel("Loan Amount Term Relation", plotOutput("fundedAmtIncomeAndInterestRelation", height="700px")),
-                tabPanel("Loan Amount Term Relation", plotOutput("loanFundedAmtByState", height="700px"))
+                tabPanel("Year wise loans trend", plotOutput("loanProcessesedEachYear")%>% withSpinner(color="#0dc5c1"), plotOutput("totalFundedLoanAmountEachYear")),
+                tabPanel("Loan Amount Term Relation", plotOutput("loanAmtTermRelation", height="700px")%>% withSpinner(color="#0dc5c1") ),
+                tabPanel("Loan Funded Amount, Income, Interest Relation", plotOutput("fundedAmtIncomeAndInterestRelation", height="700px")%>% withSpinner(color="#0dc5c1")),
+                tabPanel("Loan Amount Funded by state", plotOutput("loanFundedAmtByState", height="700px")%>% withSpinner(color="#0dc5c1"))
             )
         )
     )
